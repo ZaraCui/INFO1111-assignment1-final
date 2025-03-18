c862e3ca1a07a8cfeea5341c737b6ef534ba90c4
ZaraCui <zaracuijiajie@gmail.com>
2025-03-18 05:55:57 +0000

    Manually add working Next.js structure

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <div>
+        <h1>Hello INFO1111!</h1>
+        <p>This is a working Next.js homepage using Page Router.</p>
+      </div>
+    );
+  }
+  
