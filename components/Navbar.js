84108ee4130fcaf1984fbd2aa7b69fb096af4d12
ZaraCui <zaracuijiajie@gmail.com>
2025-03-21 19:11:50 +1100

    navbar changes

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,13 +3,13 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <nav className="bg-white shadow px-6 py-4 flex gap-6 text-sm font-medium">
-      <Link href="/" className="hover:text-blue-600">Dashboard</Link>
-      <Link href="/residents" className="hover:text-blue-600">Residents</Link>
-      <Link href="/documents" className="hover:text-blue-600">Documents</Link>
-      <Link href="/finances" className="hover:text-blue-600">Finances</Link>
-      <Link href="/maintenance" className="hover:text-blue-600">Maintenance</Link>
-      <Link href="/announcements" className="hover:text-blue-600">Announcements</Link>
-      <Link href="/submit-issue" className="hover:text-blue-600">Submit Issue</Link>
+      <Link href="/">Dashboard</Link>
+      <Link href="/residents">Residents</Link>
+      <Link href="/documents">Documents</Link>
+      <Link href="/finances">Finances</Link>
+      <Link href="/maintenance">Maintenance</Link>
+      <Link href="/announcements">Announcements</Link>
+      <Link href="/submit-issue">Submit Issue</Link>
     </nav>
   );
 }
