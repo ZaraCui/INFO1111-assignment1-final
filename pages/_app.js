5628c4a60e61d74793e7b1a068a7f189cf58eb74
ZaraCui <zaracuijiajie@gmail.com>
2025-03-26 11:32:33 +0000

    change

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
-export default function SubmitIssue() {
+import Navbar from "../components/Navbar";
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }) {
   return (
-    <main className="p-6">
-      <h1 className="text-2xl font-bold mb-4">Submit a Maintenance Issue</h1>
-      <form>
-        {/* Your form fields here */}
-      </form>
-    </main>
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
   );
 }
+
+export default MyApp;
