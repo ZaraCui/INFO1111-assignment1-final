31fd1cadd00c33dfb14d62766d89310acb4c9337
ZaraCui <zaracuijiajie@gmail.com>
2025-04-25 01:17:27 +0000

    .

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,7 @@
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Navbar />
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
