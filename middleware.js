2f018db65575eca0d853d6aca2dd335f6f53f45d
ZaraCui <zaracuijiajie@gmail.com>
2025-04-26 09:14:41 +0000

    .

--- middleware.js
+++ middleware.js
@@ -1,7 +1,14 @@
 import { authMiddleware } from "@clerk/nextjs/server";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/sign-in(.*)", "/sign-up(.*)", "/submit-issue"],
+  publicRoutes: [
+    "/", 
+    "/sign-in(.*)", 
+    "/sign-up(.*)", 
+    "/submit-issue",
+    "/contact", 
+    "/api/(.*)" 
+  ],
 });
 
 export const config = {
