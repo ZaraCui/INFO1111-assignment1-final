d49ddd8781abdf8e484bbcda62d2da093d133053
ZaraCui <zaracuijiajie@gmail.com>
2025-04-26 06:32:43 +0000

    .

--- pages/_app.js
+++ pages/_app.js
@@ -2,17 +2,15 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { useRouter } from "next/router";
 import "../styles/globals.css";
 
-const publicPages = ["/", "/submit-issue"];
-
 function MyApp({ Component, pageProps }) {
-  const { pathname } = useRouter();
-  const isPublicPage = publicPages.includes(pathname);
+  const router = useRouter();
 
   return (
     <ClerkProvider
       publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
       navigateAfterSignIn="/"
       navigateAfterSignUp="/"
+      routerPush={(to) => router.push(to)}
     >
       <Component {...pageProps} />
     </ClerkProvider>
