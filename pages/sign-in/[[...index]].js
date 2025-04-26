7b2d2f8750f7151c64e97d6ffe19e46c9d6113c5
ZaraCui <zaracuijiajie@gmail.com>
2025-04-26 05:55:46 +0000

    .

--- pages/sign-in/[[...index]].js
+++ pages/sign-in/[[...index]].js
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function SignInPage() {
   return (
-    <div className="min-h-screen flex justify-center items-center">
+    <div className="h-screen w-screen flex items-center justify-center">
       <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
     </div>
   );
--- pages/sign-up/[[...index]].js
+++ pages/sign-up/[[...index]].js
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function SignUpPage() {
   return (
-    <div className="min-h-screen flex justify-center items-center">
+    <div className="h-screen w-screen flex items-center justify-center">
       <SignUp path="/sign-up" routing="path" signInUrl="/sign-in" />
     </div>
   );
