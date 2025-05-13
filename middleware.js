263074c7fd6d04e8314fd72b58c70ee3a828941c
ZaraCui <zaracuijiajie@gmail.com>
2025-05-13 22:19:10 +0000

    fix: use minimal Clerk middleware for v6 compatibility

--- middleware.js
+++ middleware.js
@@ -1,11 +1,9 @@
 import { withClerkMiddleware } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-const isDev = process.env.NODE_ENV !== "production";
-
-export default isDev
-  ? () => NextResponse.next()
-  : withClerkMiddleware(() => NextResponse.next());
+export default withClerkMiddleware(() => {
+  return NextResponse.next();
+});
 
 export const config = {
   matcher: ["/((?!_next|.*\\..*).*)"],
