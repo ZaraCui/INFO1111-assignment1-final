672b067b37370e67f843fd1cc200beca634784c9
ZaraCui <zaracuijiajie@gmail.com>
2025-05-13 21:57:22 +0000

    .

--- middleware.js
+++ middleware.js
@@ -1,9 +1,10 @@
 import { authMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-const isDev = process.env.NODE_ENV !== "production"; 
+const isDev = process.env.NODE_ENV !== "production";
 
 export default isDev
-  ? (req) => req.next()  
+  ? () => NextResponse.next()  
   : authMiddleware({
       publicRoutes: ["/", "/sign-in(.*)", "/sign-up(.*)", "/submit-issue"],
     });
